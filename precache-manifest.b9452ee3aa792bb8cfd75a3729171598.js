fe29db5b8ac06e36911849df40edabfe66926a27
guiialmeiida <gui.almeidacrz@gmail.com>
2020-06-13 11:22:27 -0300

    Updates

---
+++ precache-manifest.b9452ee3aa792bb8cfd75a3729171598.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a261f2b684b18862898af351f9b6df83",
+    "url": "/teachersroom/index.html"
+  },
+  {
+    "revision": "45660681af5099ae1d09",
+    "url": "/teachersroom/static/js/2.d6fc4cae.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/teachersroom/static/js/2.d6fc4cae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c26e00364ae8eaf7d4eb",
+    "url": "/teachersroom/static/js/main.b82f0d6e.chunk.js"
+  },
+  {
+    "revision": "dbd7f8b2e7e1a330f6d8",
+    "url": "/teachersroom/static/js/runtime-main.13fbd1aa.js"
+  },
+  {
+    "revision": "2e46ccef417a29fe41503e80a4b41059",
+    "url": "/teachersroom/static/media/about.2e46ccef.jpg"
+  }
+]);
